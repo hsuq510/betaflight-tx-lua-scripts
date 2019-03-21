9fed6b35f0fc9c49fb2a25b67e41ab71145ab22c
madec <husq510@gmail.com>
2019-03-21 12:13:01 +0100

    Update rates4.lua

--- src/SCRIPTS/BF/X7/rates4.lua
+++ src/SCRIPTS/BF/X7/rates4.lua
@@ -12,12 +12,11 @@ return {
       { t = "Iterm Rot.",      x = 5, y = 20, to = SMLSIZE },
       { t = "Iterm Relax",      x = 5, y = 28, to = SMLSIZE },
       { t = "Iterm Rlx Ty",      x = 5, y = 36, to = SMLSIZE },
-      { t = "Iterm Rlx CO",      x = 5, y = 44, to = SMLSIZE },
    },
    fields = {
       { x = 70, y = 12, min = 0,   max = 100,  vals = { 31 }, to = SMLSIZE },
       { x = 70, y = 20, min = 0,    max = 1,     vals = { 26 },      to = SMLSIZE, table = { [0]="OFF", "ON" } },
       { x = 70, y = 28, min = 0,    max = 4,     vals = { 28 },      to = SMLSIZE, table = { [0]="OFF", "RP","RPY","RPINC","RPYINC" } },
-      { x = 70, y = 36, min = 0,    max = 1,     vals = { 29 },      to = SMLSIZE, table = { [0]="GYRO", "SET" } },
+      { x = 70, y = 36, min = 0,    max = 1,     vals = { 29 },      to = SMLSIZE, table = { [0]="GYRO", "SETP" } },
    }
 }
