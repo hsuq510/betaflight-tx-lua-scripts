ad51e5f99a2fe50c2670ede5ac9ff7398412794a
madec <husq510@gmail.com>
2019-03-21 12:08:52 +0100

    Update rates3.lua

--- src/SCRIPTS/BF/X7/rates3.lua
+++ src/SCRIPTS/BF/X7/rates3.lua
@@ -2,7 +2,7 @@
 return {
    read           = 94, -- MSP_PID_ADVANCED
    write          = 95, -- MSP_SET_PID_ADVANCED
-   title          = "Rates (3/4)",
+   title          = "RC Tuning",
    reboot         = false,
    eepromWrite    = true,
    minBytes       = 39,
@@ -18,7 +18,7 @@ return {
       { x = 70, y = 12, min = 0,   max = 20,  vals = { 30 }, to = SMLSIZE },
       { x = 70, y = 20, min = 1000, max = 30000, vals = { 22, 23 }, to = SMLSIZE, scale = 1000, mult = 100 },
       { x = 70, y = 28, min = 20,   max = 1000,  vals = { 20, 21 }, to = SMLSIZE },
-      { x = 70, y = 36, min = 0,    max = 1,     vals = { 39 },      to = SMLSIZE, table = { [0]="SMUT", "STEP" } },
+      { x = 70, y = 36, min = 0,    max = 1,     vals = { 39 },      to = SMLSIZE, table = { [0]="SMOT", "STEP" } },
       { x = 70, y = 44, min = 0,    max = 1,     vals = { 8 },      to = SMLSIZE, table = { [0]="OFF", "ON" } },
    }
 }
