66a72afcf677e63d9d8642090b6cb5958586565f
madec <husq510@gmail.com>
2019-04-01 14:12:43 +0200

    Update rates4.lua

--- src/SCRIPTS/BF/X7/rates4.lua
+++ src/SCRIPTS/BF/X7/rates4.lua
@@ -7,6 +7,7 @@ return {
    eepromWrite    = true,
    minBytes       = 39,
    outputBytes    = 39,
+   requiredVersion = 1.040,
    text = {
       { t = "Thr. Boost",      x = 5, y = 12, to = SMLSIZE },
       { t = "Iterm Rot.",      x = 5, y = 20, to = SMLSIZE },
