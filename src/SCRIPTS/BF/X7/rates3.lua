550d0932de65a278f9179f6fc71934c1eefbf8ab
madec <husq510@gmail.com>
2019-04-01 14:17:55 +0200

    Update rates3.lua

--- src/SCRIPTS/BF/X7/rates3.lua
+++ src/SCRIPTS/BF/X7/rates3.lua
@@ -8,14 +8,12 @@ return {
    minBytes       = 39,
    outputBytes    = 39,
    text = {
-      { t = "ABS Control",      x = 5, y = 12, to = SMLSIZE },
       { t = "AG Gain",      x = 5, y = 20, to = SMLSIZE },
       { t = "AG Threshold", x = 5, y = 28, to = SMLSIZE },
       { t = "AG Mode", x = 5, y = 36, to = SMLSIZE },
       { t = "VBAT Comp.",      x = 5, y = 44, to = SMLSIZE },
    },
    fields = {
-      { x = 70, y = 12, min = 0,   max = 20,  vals = { 30 }, to = SMLSIZE },
       { x = 70, y = 20, min = 1000, max = 30000, vals = { 22, 23 }, to = SMLSIZE, scale = 1000, mult = 100 },
       { x = 70, y = 28, min = 20,   max = 1000,  vals = { 20, 21 }, to = SMLSIZE },
       { x = 70, y = 36, min = 0,    max = 1,     vals = { 39 },      to = SMLSIZE, table = { [0]="SMOT", "STEP" } },
